83ff3a7f01de8012db28b5e534ea85f84253d144
mayong <mayong@qianxin.com>
2023-02-25 19:34:29 +0800

    Add cpp_onnxruntime

---
+++ cpp_onnx/tester/tester.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#ifndef _WIN32
+#include <sys/time.h>
+#else
+#include <win_func.h>
+#endif
+
+#include <Audio.h>
+#include <Model.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+    if (argc < 2)
+    {
+        printf("Usage: %s /path/to/model_dir /path/to/wav/file", argv[0]);
+        exit(-1);
+    }
+    struct timeval start, end;
+    Audio audio(0);
+    audio.loadwav(argv[2]);
+    audio.disp();
+    gettimeofday(&start, NULL);
+    Model *mm = create_model(argv[1], 3);
+    gettimeofday(&end, NULL);
+    long seconds = (end.tv_sec - start.tv_sec);
+    long micros = ((seconds * 1000000) + end.tv_usec) - (start.tv_usec);
+    printf("Model initialization takes %lfs.\n", (double)micros / 1000000);
+    audio.split();
+
+    setbuf(stdout, NULL);
+    cout << "Result: \"";
+    gettimeofday(&start, NULL);
+    float *buff;
+    int len;
+    int flag;
+    while (audio.fetch(buff, len, flag) > 0) {
+        mm->reset();
+        string msg = mm->forward(buff, len, flag);
+        cout << msg;
+    }
+
+    gettimeofday(&end, NULL);
+
+    cout << "\"." << endl;
+
+    seconds = (end.tv_sec - start.tv_sec);
+    micros = ((seconds * 1000000) + end.tv_usec) - (start.tv_usec);
+    printf("Model inference takes %lfs.\n", (double)micros / 1000000);
+    delete mm;
+
+    return 0;
+}
---
+++ cpp_onnx/win/readme.md
@@ -0,0 +1,28 @@
+# 最简编译指南
+
+取下代码后用Vs2022 打开cmakelists.txt 编译即可。
+
+注意：在安装vs2022时，需要安装linux 开发组件以支持cmakelists.txt工程。
+
+
+## Demo
+CPU:  i7-7820HQ @ Windows 11
+
+<div align="center">
+    <img src="images/sample.png" width="80%" height="80%">
+</div>
+
+
+
+## 准备windows依赖库 （专业用户参考，普通用户无需考虑）
+
+
+
+### build fftw3 (from a prebuild version to generate libs)
+
+https://blog.csdn.net/NICAI001/article/details/116887842
+
+
+### openblas
+
+https://github.com/xianyi/OpenBLAS/releases
